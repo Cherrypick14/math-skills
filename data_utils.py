65f7a01f584480924f8f4db699685a7aaa8a1269
Cherrypick14 <cherrypicklet@gmail.com>
2024-05-29 07:49:45 +0300

    Fix more typos in the code

--- data_utils.py
+++ data_utils.py
@@ -12,6 +12,7 @@ def read_data(file_path):
         
         with open(file_path,'r') as file:
             data = [int(line.strip()) for line in file]
+        
            
         return data 
     except FileNotFoundError as fnf_error:
